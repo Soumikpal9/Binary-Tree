5b34f84c4a0bd85fd256e5531948871980c66e87
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-14 09:25:41 +0530

    [SP]Added: Three Nodes Added To Binary Search Tree

--- src/com/cg/Executor.java
+++ src/com/cg/Executor.java
@@ -3,5 +3,13 @@ package com.cg;
 public class Executor {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Binary Tree Problem!!!");
+		
+		BinaryTree<Integer> binaryTree = new BinaryTree<>();
+		binaryTree.add(50);
+		binaryTree.add(36);
+		binaryTree.add(70);
+		
+		int size = binaryTree.getSize();
+		System.out.println(size);
 	}
 }
